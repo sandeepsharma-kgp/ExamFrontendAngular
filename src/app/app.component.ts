585a4fc7a73a865d6b2f89a201c398002c9827de
Harsh Khirwal <harshkhirwal24@gmail.com>
2019-06-29 13:08:59 +0530

    Login route added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// class HeaderComponent {
+//   // constructor(private router: Routes) {} (2)
+//   //
+//   // login() {
+//   //   this.router.navigate(['login']); (3)
+//   // }
+//
+//   // goSearch() {
+//   //   this.router.navigate(['search']); (3)
+//   // }
+// }
+
 export class AppComponent {
   title = 'frontend';
 }
